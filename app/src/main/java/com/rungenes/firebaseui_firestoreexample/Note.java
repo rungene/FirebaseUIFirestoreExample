210a831ea1bad537a49c68fad0e2276814b18578
rungene <rungenesamuel@gmail.com>
2018-08-13 21:10:54 +0300

    Initial commit

---
+++ app/src/main/java/com/rungenes/firebaseui_firestoreexample/Note.java
@@ -0,0 +1,33 @@
+package com.rungenes.firebaseui_firestoreexample;
+
+public class Note {
+
+    private String title;
+    private int  priority;
+    private String description;
+
+    public Note(){
+
+
+    }
+
+    public Note(String title, int priority, String description) {
+        this.title = title;
+        this.priority = priority;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+}
